7b51f1a769de0bc94f57f1c505902f47fa6dc419
samyak jain <43640144+samyak1512@users.noreply.github.com>
2023-01-20 00:07:28 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Samyak Jain`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! Samyak Jain`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Data Scientist", "Web Designer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>I am a sophomore at NSUT. I am majoring in Computer Science and aspire to be an upcoming data scientist.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
